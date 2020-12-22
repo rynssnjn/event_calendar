fffc6be4b0ece26a52084e8510d7742df4700994
Rael San Juan <rynssnjn@gmail.com>
2020-12-22 17:29:06 +0800

    refactor

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -50,66 +50,57 @@ class _MyHomePageState extends State<MyHomePage> {
     EventModel event = EventModel(
       onSelect: () => print('Action Tapped: Meeting 1'),
       title: 'Meeting 1',
-      startTime: DateTime(2020, 11, 01),
-      endTime: DateTime(2020, 12, 10),
+      startTime: DateTime(2020, 11, 1),
+      endTime: DateTime(2020, 12, 5),
       backgroundColor: Color(0xff31C987),
     );
     events.add(event);
 
-    EventModel event2 = EventModel(
-      onSelect: () => print('Action Tapped: Meeting 2'),
-      title: 'Meeting 2',
-      startTime: DateTime(2020, 11, 01),
-      endTime: DateTime(2020, 12, 10),
-      backgroundColor: Color(0xff892486),
-    );
-    events.add(event2);
-
     EventModel event3 = EventModel(
       title: 'Meeting 3',
-      startTime: DateTime(2020, 11, 01),
-      endTime: DateTime(2020, 12, 10),
+      startTime: DateTime(2020, 12, 3),
+      endTime: DateTime(2020, 12, 09),
       backgroundColor: Color(0xff892486),
     );
     events.add(event3);
 
     EventModel event4 = EventModel(
       title: 'Meeting 4',
-      startTime: DateTime(2020, 12, 01),
-      endTime: DateTime(2020, 12, 09),
+      startTime: DateTime(2020, 12, 09),
+      endTime: DateTime(2020, 12, 12),
       backgroundColor: Color(0xff892486),
     );
     events.add(event4);
 
     EventModel event5 = EventModel(
       title: 'Meeting 5',
-      startTime: DateTime(2020, 12, 09),
+      startTime: DateTime(2020, 12, 12),
       endTime: DateTime(2020, 12, 12),
-      backgroundColor: Color(0xff31C987),
+      backgroundColor: Color(0xff892486),
     );
     events.add(event5);
 
     EventModel event6 = EventModel(
       title: 'Meeting 6',
       startTime: DateTime(2020, 12, 12),
-      endTime: DateTime(2020, 12, 12),
-      backgroundColor: Color(0xff892486),
+      endTime: DateTime(2020, 12, 15),
+      backgroundColor: Color(0xff31C987),
     );
     events.add(event6);
 
     EventModel event7 = EventModel(
       title: 'Meeting 7',
-      startTime: DateTime(2020, 12, 12),
-      endTime: DateTime(2020, 12, 12),
-      backgroundColor: Color(0xff31C987),
+      startTime: DateTime(2020, 12, 6),
+      endTime: DateTime(2020, 12, 7),
+      backgroundColor: Color(0xff892486),
     );
     events.add(event7);
 
     EventModel event8 = EventModel(
       title: 'Meeting 8',
-      startTime: DateTime(2020, 12, 10),
-      endTime: DateTime(2020, 12, 11),
-      backgroundColor: Color(0xff892486),
+      startTime: DateTime(2020, 12, 12),
+      endTime: DateTime(2020, 12, 12),
+      backgroundColor: Color(0xff31C987),
     );
     events.add(event8);
 
@@ -123,27 +114,43 @@ class _MyHomePageState extends State<MyHomePage> {
 
     EventModel event10 = EventModel(
       title: 'Meeting 10',
-      startTime: DateTime(2020, 12, 12),
-      endTime: DateTime(2020, 12, 12),
+      startTime: DateTime(2020, 12, 13),
+      endTime: DateTime(2020, 12, 18),
       backgroundColor: Color(0xff31C987),
     );
     events.add(event10);
 
     EventModel event11 = EventModel(
       title: 'Meeting 11',
-      startTime: DateTime(2020, 12, 13),
-      endTime: DateTime(2020, 12, 18),
-      backgroundColor: Color(0xff31C987),
+      startTime: DateTime(2020, 12, 15),
+      endTime: DateTime(2020, 12, 22),
+      backgroundColor: Color(0xff892486),
     );
     events.add(event11);
 
     EventModel event12 = EventModel(
       title: 'Meeting 12',
-      startTime: DateTime(2020, 12, 15),
-      endTime: DateTime(2020, 12, 22),
-      backgroundColor: Color(0xff892486),
+      startTime: DateTime(2020, 12, 21),
+      endTime: DateTime(2020, 12, 24),
+      backgroundColor: Color(0xff31C987),
     );
     events.add(event12);
+
+    EventModel event13 = EventModel(
+      title: 'Meeting 13',
+      startTime: DateTime(2020, 12, 10),
+      endTime: DateTime(2020, 12, 10),
+      backgroundColor: Color(0xff31C987),
+    );
+    events.add(event13);
+
+    EventModel event14 = EventModel(
+      title: 'Meeting 14',
+      startTime: DateTime(2020, 12, 8),
+      endTime: DateTime(2020, 12, 10),
+      backgroundColor: Color(0xff31C987),
+    );
+    events.add(event14);
 
     EventModel.setListAndUpdateMap(events);
   }
